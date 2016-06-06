9d9472e38552a3ada35bcb9585396af8802db1d0
Steve Kemp <steve@steve.org.uk>
2016-06-06 05:31:33 +0300

    Added PoD

--- lib/DHCP/Lookup.pm
+++ lib/DHCP/Lookup.pm
@@ -1,9 +1,59 @@
+
+=head1 NAME
+
+DHCP::Lookup - Perform DNS lookups.
+
+=head1 DESCRIPTION
+
+This module is used to perform DNS lookups of given hostnames, it is
+used in our code to show the B<existing> value(s) for a given name.
+
+For example we'll lookup the value of C<skx.dhcp.io> to return:
+
+=for example begin
+
+     $result->{'a'}   = "127.0.0.1";
+     $result->{'aaaa'} = "fe80::1";
+
+=for example end
+
+Our code used to query the current values by querying the Amazon
+Route53 infrastructure directly, this is admittedly more reliable
+but the overhead was too high given the size of our domain.
+
+=cut
+
+=head1 AUTHOR
+
+Steve Kemp <steve@steve.org.uk>
+
+=cut
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2016 Steve Kemp <steve@steve.org.uk>.
+
+This library is free software. You can modify and or distribute it under
+the same terms as Perl itself.
+
+=cut
+
+
 package DHCP::Lookup;
 
 use strict;
 use warnings;
 
 use Net::DNS::Resolver;
+
+
+=begin doc
+
+Constructor
+
+=end doc
+
+=cut
 
 sub new
 {
@@ -15,6 +65,16 @@ sub new
     bless( $self, $class );
     return $self;
 }
+
+
+=begin doc
+
+Lookup the values of the given name.  We use type C<any> when making
+the query, such that we can discover both C<A> and C<AAAA> records.
+
+=end doc
+
+=cut
 
 sub values
 {
