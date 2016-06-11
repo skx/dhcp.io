7ab50bab63d9ecf506bcb6239ab9a99b3eb6aa7c
Steve Kemp <steve@steve.org.uk>
2016-06-11 04:41:17 +0300

    Updated to implement bcrypt-based password handling.
    This does two things:
    
    * Tests via the new-style bcrypt hash, if present.
    * If only a legacy password is present upgrades to bcrypt on valid-login.
    
    Updates #24.

--- lib/DHCP/User/Auth.pm
+++ lib/DHCP/User/Auth.pm
@@ -46,6 +46,7 @@ use warnings;
 
 package DHCP::User::Auth;
 
+use Crypt::Eksblowfish::Bcrypt;
 use DHCP::Config;
 use Singleton::DBI;
 
@@ -98,8 +99,7 @@ sub test_login
         if ($result)
         {
             #
-            # Set the password here - so that we migrate
-            # to bcrypt.
+            # Update to the new bcrypt password hash here.
             #
             $self->set_password( username => $params{ 'username' },
                                  password => $params{ 'password' } );
@@ -131,25 +131,63 @@ sub set_password
     # table - so we don't need to make this conditional
     # at all
     #
+    my $db = Singleton::DBI->instance() || die "Missing DB-handle";
+    my $sql = $db->prepare("SELECT id FROM users WHERE login=?") or
+      die "Failed to prepare statement";
+    $sql->execute($user) or
+      die "Failed to execute statement";
+    my $user_id = $sql->fetchrow_array();
+    $sql->finish();
 
 
     #
-    #  Find the user_id
+    #  Delete the old value
     #
+    $sql = $db->prepare("DELETE FROM passwords WHERE owner=?");
+    $sql->execute($user_id) or
+      die "Failed to delete password";
+    $sql->finish();
 
     #
-    #  Delete any existing record
+    #  If there is a legacy-password, then empty that here.
     #
+    #  TODO: Remove this when all users have migrated.
+    #
+    $sql = $db->prepare("UPDATE users SET password=? WHERE id=?") or
+      die "Failed to prepare";
+    $sql->execute( "bcrypt", $user_id ) or
+      die "Failed to update legacy password";
+    $sql->finish();
+
+    #
+    #  Calculate the hash
+    #
+    my $salt = "";
+    my @chars = ( "A" .. "Z", "a" .. "z" );
+    $salt .= $chars[rand @chars] for 1 .. 16;
+
+    #
+    #  Now hash the user's passwrod
+    #
+    $salt = Crypt::Eksblowfish::Bcrypt::en_base64($salt);
+    my $settings = '$2a$12$' . $salt;
+    my $hash = Crypt::Eksblowfish::Bcrypt::bcrypt( $pass, $settings );
 
     #
     #  Set the password.
     #
+    $sql = $db->prepare("INSERT INTO passwords (hash,owner) VALUES(?,?)") or
+      die "Failed to prepare";
+    $sql->execute( $hash, $user_id ) or
+      die "Failed to add password";
+
+    $sql->finish();
 }
 
 
 =begin doc
 
-Has this user got a new-style, salted, password?
+Has this user got a new-style bcrypt-based password?
 
 =end doc
 
@@ -219,8 +257,7 @@ sub test_login_old
 
 =begin doc
 
-Test the given login/password for validity with the new-style
-bcrypt setup.
+Test the given login/password for validity against the (new) bycrypt hash.
 
 =end doc
 
@@ -229,6 +266,55 @@ bcrypt setup.
 sub test_login_new
 {
     my ( $self, %params ) = (@_);
+
+    my $user = $params{ 'username' };
+    my $pass = $params{ 'password' };
+
+    #
+    #  Select the hash we have stored for the user.
+    #
+    my $dbh = Singleton::DBI->instance();
+    my $sql = $dbh->prepare(
+        "SELECT a.hash FROM passwords AS a JOIN users AS b WHERE a.owner=b.id AND b.login=?"
+      ) or
+      die "Failed to prepare " . $dbh->errstr();
+    $sql->execute($user);
+
+    my ($hash) = $sql->fetchrow_array();
+    $sql->finish();
+
+    my $salt = undef;
+    if ( $hash =~ m!^\$2a\$\d{2}\$([A-Za-z0-9+\\.\/]{22})! )
+    {
+        $salt = $1;
+    }
+    else
+    {
+        # This shouldn't happen..
+        return undef;
+    }
+
+
+    #
+    #  Now hash the user's passwrod
+    #
+    my $settings = '$2a$12$' . $salt;
+    my $out = Crypt::Eksblowfish::Bcrypt::bcrypt( $pass, $settings );
+
+
+    #
+    #  Does that match?
+    #
+    #  NOTE: Ideally we'd use a constant-time comparison here.
+    #
+    if ( $out eq $hash )
+    {
+        return $user;
+    }
+    else
+    {
+        return undef;
+    }
 }
 
 
